9bf67dfce0220106d7ac459d6648121c53b68bf7
RoySmith <roysmith@spi-tools-host-1.spi-tools.eqiad1.wikimedia.cloud>
2021-04-22 02:24:07 +0000

    Refactor do_page().

--- no-refs.py
+++ no-refs.py
@@ -10,14 +10,19 @@ import sys
 
 DUMP_ROOT = '/public/dumps/public/enwiki'
 
+PAGE = object()
+
 class Finder:
     def __init__(self):
+        self.path = None
         self.file_count = 0
         self.page_count = 0
         self.article_count = 0
         self.blp_count = 0
         self.found = 0
         self.t0 = datetime.now()
+        self.path = None
+        self.doc = None
 
     def process_directory(self, dump_name):
         dump_dir = Path(DUMP_ROOT) / dump_name
@@ -34,32 +39,37 @@ class Finder:
 
 
     def process_file(self, path):
+        self.path = path
         stream = bz2.open(path) if path.endswith('.bz2') else open(path)
-        doc = pulldom.parse(stream)
+        self.doc = pulldom.parse(stream)
         title = None
-        for event, node in doc:
+        for event, node in self.doc:
             if event == pulldom.START_ELEMENT and node.tagName == 'page':
-                self.progress()
-                doc.expandNode(node)
-                ns = self.get_text_from_singleton_node(node, 'ns')
-                title_nodes = node.getElementsByTagName('title')
-                assert len(title_nodes) == 1
-                cdata_nodes = title_nodes[0].childNodes
-                title = self.get_text_from_singleton_node(node, 'title')
-                if not ns == '0':
-                    continue
+                self.do_page(node)
 
-                self.article_count += 1
-                content = self.get_text_from_singleton_node(node, 'text').lower()
-                if '#redirect' in content:
-                    continue
 
-                if 'living people' in content:
-                    self.blp_count += 1
-                    if not 'ref' in content:
-                        self.found += 1
-                        print(title)
-                        logging.info(f'Found "{title}" in {path}')
+    def do_page(self, node):
+        self.progress()
+        self.doc.expandNode(node)
+        if self.get_text_from_singleton_node(node, 'ns') != '0':
+            return
+
+        title_nodes = node.getElementsByTagName('title')
+        assert len(title_nodes) == 1
+        cdata_nodes = title_nodes[0].childNodes
+        title = self.get_text_from_singleton_node(node, 'title')
+
+        self.article_count += 1
+        content = self.get_text_from_singleton_node(node, 'text').lower()
+        if '#redirect' in content:
+            return
+
+        if 'living people' in content:
+            self.blp_count += 1
+            if not 'ref' in content:
+                self.found += 1
+                print(title)
+                logging.info(f'Found "{title}" in {self.path}')
 
 
     def get_text_from_singleton_node(self, node, tag):
