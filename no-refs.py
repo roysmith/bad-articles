67970a2e6d1bb21b4205e8e3a7c602deb1074652
RoySmith <roysmith@spi-tools-host-1.spi-tools.eqiad1.wikimedia.cloud>
2021-04-25 23:54:14 +0000

    Convert to pure state-driven architecture.

--- no-refs.py
+++ no-refs.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from argparse import ArgumentParser
 import re
 import bz2
-from xml.dom import pulldom
+from xml.dom.pulldom import parse, START_DOCUMENT, START_ELEMENT, END_ELEMENT, CHARACTERS
 from datetime import datetime
 import humanize
 import logging
@@ -12,7 +12,19 @@ DUMP_ROOT = '/public/dumps/public/enwiki'
 
 PAGE = object()
 
+class Page:
+    def __init__(self):
+        self.revisions = []
+
+
+class Revision:
+    pass
+
+
 class Finder:
+    """docstring"""
+
+
     def __init__(self):
         self.path = None
         self.file_count = 0
@@ -23,6 +35,19 @@ class Finder:
         self.t0 = datetime.now()
         self.path = None
         self.doc = None
+        self.page_data = None
+        self.state = [self.start]
+
+
+    def push(self, state):
+        logging.debug(f"push({state})")
+        self.state.append(state)
+
+
+    def pop(self):
+        state = self.state.pop()
+        logging.debug(f"pop({state})")
+
 
     def process_directory(self, dump_name):
         dump_dir = Path(DUMP_ROOT) / dump_name
@@ -34,33 +59,67 @@ class Finder:
         paths.sort()
 
         for key, path in paths:
-            self.file_count += 1
             self.process_file(str(path))
 
 
     def process_file(self, path):
         self.path = path
         stream = bz2.open(path) if path.endswith('.bz2') else open(path)
-        self.doc = pulldom.parse(stream)
-        title = None
+        self.doc = parse(stream)
+        self.cdata = []
         for event, node in self.doc:
-            if event == pulldom.START_ELEMENT and node.tagName == 'page':
-                self.do_page(node)
+            logging.debug(f"{list(s.__name__ for s in self.state)}: {event}, {node}")
+            if event == CHARACTERS:
+                self.cdata.append(node.data)
+                continue
+            if event == START_ELEMENT:
+                self.cdata = []
+            self.state[-1](event, node)
+        self.file_count += 1
 
 
-    def do_page(self, node):
-        self.progress()
-        self.doc.expandNode(node)
-        if self.get_text_from_singleton_node(node, 'ns') != '0':
+    def start(self, event, node):
+        if event == START_DOCUMENT:
+            self.push(self.document)
+
+
+    def document(self, event, node):
+        if event == START_ELEMENT and node.tagName == 'mediawiki':
+            self.push(self.mediawiki)
+
+
+    def mediawiki(self, event, node):
+        if event == END_ELEMENT and node.tagName == 'mediawiki':
+            self.pop()
+        if event == START_ELEMENT and node.tagName == 'page':
+            self.page_data = Page()
+            self.push(self.page)
+
+
+    def page(self, event, node):
+        if event == END_ELEMENT and node.tagName == 'page':
+            self.do_page()
+            self.page_data = None
+            self.pop()
+        if event == START_ELEMENT and node.tagName == 'title':
+            self.push(self.title)
+        if event == START_ELEMENT and node.tagName == 'ns':
+            self.push(self.ns)
+        if event == START_ELEMENT and node.tagName == 'revision':
+            self.page_data.revisions.append(Revision())
+            self.push(self.revision)
+
+
+    def do_page(self):
+        self.page_count += 1
+        if self.page_count % 1000 == 0:
+            self.log_progress()
+
+        if self.page_data.ns != '0':
             return
 
-        title_nodes = node.getElementsByTagName('title')
-        assert len(title_nodes) == 1
-        cdata_nodes = title_nodes[0].childNodes
-        title = self.get_text_from_singleton_node(node, 'title')
-
         self.article_count += 1
-        content = self.get_text_from_singleton_node(node, 'text').lower()
+        content = self.page_data.revisions[-1].text.lower()
         if '#redirect' in content:
             return
 
@@ -68,8 +127,49 @@ class Finder:
             self.blp_count += 1
             if not 'ref' in content:
                 self.found += 1
+                title = self.page_data.title
                 print(title)
                 logging.info(f'Found "{title}" in {self.path}')
+
+
+    def title(self, event, node):
+        if event == END_ELEMENT and node.tagName == 'title':
+            self.page_data.title = ''.join(self.cdata)
+            self.pop()
+
+
+    def ns(self, event, node):
+        if event == END_ELEMENT and node.tagName == 'ns':
+            self.page_data.ns = ''.join(self.cdata)
+            self.pop()
+
+
+    def revision(self, event, node):
+        if event == END_ELEMENT and node.tagName == 'revision':
+            self.pop()
+        if event == START_ELEMENT and node.tagName == 'id':
+            self.push(self.id)
+        if event == START_ELEMENT and node.tagName == 'contributor':
+            self.push(self.contributor)
+        if event == START_ELEMENT and node.tagName == 'text':
+            self.push(self.text)
+
+
+    def text(self, event, node):
+        if event == END_ELEMENT and node.tagName == 'text':
+            self.page_data.revisions[-1].text = ''.join(self.cdata)
+            self.pop()
+
+
+    def contributor(self, event, node):
+        if event == END_ELEMENT and node.tagName == 'contributor':
+            self.pop()
+
+
+    def id(self, event, node):
+        if event == END_ELEMENT and node.tagName == 'id':
+            self.page_data.revisions[-1].id = ''.join(self.cdata)
+            self.pop()
 
 
     def get_text_from_singleton_node(self, node, tag):
@@ -78,11 +178,6 @@ class Finder:
         cdata_nodes = children[0].childNodes
         return ''.join(n.nodeValue for n in cdata_nodes)
 
-
-    def progress(self):
-        self.page_count += 1
-        if self.page_count % 1000 == 0:
-            self.log_progress()
 
     def log_progress(self):
         dt = datetime.now() - self.t0
@@ -95,6 +190,7 @@ class Finder:
 
 def main():
     parser = ArgumentParser()
+    parser.add_argument('--log')
     input_group = parser.add_mutually_exclusive_group(required=True)
     input_group.add_argument('--file')
     input_group.add_argument('--dump_name')
@@ -102,7 +198,8 @@ def main():
     args = parser.parse_args()
 
     time_stamp = datetime.utcnow().replace(microsecond=0).isoformat()
-    logging.basicConfig(filename=f'no-refs.log.{time_stamp}',
+    logging.basicConfig(filename=args.log or f'no-refs.log.{time_stamp}',
+                        filemode='w',  # https://bugs.python.org/issue27805
                         format='%(asctime)s %(levelname)s: %(message)s',
                         level=logging.INFO)
     logging.info(f'''command line: "{' '.join(sys.argv)}"''')
